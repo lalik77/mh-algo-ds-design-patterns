7108ec80ebbdf67dde6a14e6266895075d7023d1
Alexander Mami <algru81@gmail.com>
2021-12-01 15:35:02 +0300

    Initial commit. Mvn project

---
+++ src/main/java/com/mami/App.java
@@ -0,0 +1,13 @@
+package com.mami;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
